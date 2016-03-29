7a9e09f185fc58347a56e0bdca1b5db3bb2765ce
Max Ferguson <maxkferg@gmail.com>
2016-03-29 12:53:50 -0700

    Convert readme to markdown

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
 
+# PyPi only supports rst but Github requires markdown.
+# Documentation is written in markdown and converted to rst on build
+# Pypandoc should be installed by the user who is builing this package
+# See http://stackoverflow.com/questions/10718767/have-the-same-readme-both-in-markdown-and-restructuredtext
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 setup(name='pmml',
       version='0.2',
       description='A PMML translator',
+      long_description=read_md('README.md'),
       url='http://github.com/storborg/pmml',
       author='EIG',
       author_email='pengbo.sherry@gmail.com',
